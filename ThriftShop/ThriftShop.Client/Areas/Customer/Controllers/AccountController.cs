19759fc3202814fd16da0d37752080c91c016ad0
tinhtvt97 <85776875+tinhtvt97@users.noreply.github.com>
2022-08-10 21:27:45 +0700

    Client templates

---
+++ ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThriftShop.Client.Areas.Customer.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Setting(int accountId)
+        {
+            return View();
+        }
+    }
+}
