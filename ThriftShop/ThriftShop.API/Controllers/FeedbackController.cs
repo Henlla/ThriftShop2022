d8de642db1d1f2f96eefef3c4dcd923e0e0cb566
hoaht123 <83630107+hoaht123@users.noreply.github.com>
2022-08-11 19:08:17 +0700

    Update Feedback

---
+++ ThriftShop/ThriftShop.API/Controllers/FeedbackController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThriftShop.Models;
+
+namespace ThriftShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWord;
+        public FeedbackController(IUnitOfWork _unitOfWord)
+        {
+            unitOfWord = _unitOfWord;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Feedback>> ListFeedback() {
+            return await unitOfWord.Feedback.GetAll(includeProperties: "UserInfo");
+        }
+
+        [HttpPost]
+        public async Task<Feedback> PostFeedback(Feedback obj)
+        {
+            await unitOfWord.Feedback.Add(obj);
+            unitOfWord.Save();
+            return obj;
+        }
+        [HttpPut]
+        public async Task<Feedback> PutFeedback(Feedback obj)
+        {
+            var model = unitOfWord.Feedback.GetFirstOrDefault(x => x.FeedbackId.Equals(obj.FeedbackId), includeProperties: "UserInfo");
+            if (model != null) {
+                await unitOfWord.Feedback.Update(obj);
+                unitOfWord.Save();
+                return obj;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
---
+++ ThriftShop/ThriftShop.DataAccess/IRepository/IFeedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThriftShop.DataAccess.Repository.IRepository.GenericInterface;
+using ThriftShop.Models;
+
+namespace ThriftShop.DataAccess.IRepository
+{
+    public interface IFeedback : IRepository<Feedback>
+    {
+         Task<Feedback> Update(Feedback feedback);
+    }                                                       
+    
+}
