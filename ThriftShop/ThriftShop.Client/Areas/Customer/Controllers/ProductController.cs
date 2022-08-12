d332fc23c14e5440ef1d6f3a8668152f9b6b27ea
tinhtvt97 <85776875+tinhtvt97@users.noreply.github.com>
2022-08-12 12:00:21 +0700

    test

--- ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/ProductController.cs
+++ ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/ProductController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ThriftShop.Models;
 
 namespace ThriftShop.Client.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ProductController : Controller
     {
+        private string categoryUrl = "https://localhost:7061/api/Categories/";
+        HttpClient httpClient = new HttpClient();
+        
+        public ProductController()
+        {
+
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = this.GetCategories();
+            return View(categories);
         }
         
         public IActionResult Details(int productId)
@@ -24,6 +34,12 @@ namespace ThriftShop.Client.Areas.Customer.Controllers
         public IActionResult WishList()
         {
             return View();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(httpClient.GetStringAsync(categoryUrl+ "GetAll").Result);
+            return categories;
         }
     }
 }
