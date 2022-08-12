4161fbab3a6c269b52596d467b862e56c37074e1
hoaht123 <83630107+hoaht123@users.noreply.github.com>
2022-08-13 01:22:42 +0700

    update shopping cart

--- ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/CartController.cs
+++ ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/CartController.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using ThriftShop.Models;
 
 namespace ThriftShop.Client.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class CartController : Controller
     {
+        private string urlShoppingCart = "https://localhost:7061/api/ShoppingCart/";
+        private HttpClient client = new HttpClient();
         public IActionResult Index()
         {
-            return View();
+            var objCart = JsonConvert.DeserializeObject<IEnumerable<ShoppingCart>>(client.GetStringAsync(urlShoppingCart).Result);
+            return View(objCart);
         }
 
         public IActionResult CheckOut()
         {
             return View();
         }
+
+
     }
 }
