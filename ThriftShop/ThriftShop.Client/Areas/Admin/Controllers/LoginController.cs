3829e56a4f0cb552ea7f509dc68981f0bb4f3b12
Nguyễn Tấn Hiếu <85748137+hieunguyen-18-vn@users.noreply.github.com>
2022-08-12 16:29:50 +0700

    templ
    admin temp

---
+++ ThriftShop/ThriftShop.Client/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThriftShop.Client.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class LoginController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Forgot()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAcc()
+        {
+            return View();
+        }
+
+        
+    }
+}
