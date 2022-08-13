98c2e9b110defd43c3876298b0430c4bf56b63ed
tinhtvt97 <85776875+tinhtvt97@users.noreply.github.com>
2022-08-13 16:59:19 +0700

    fix/product-add-admin-pages

--- ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/ProductController.cs
+++ ThriftShop/ThriftShop.Client/Areas/Customer/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ namespace ThriftShop.Client.Areas.Customer.Controllers
     {
         private string categoryUrl = "https://localhost:7061/api/Categories/";
         private string productUrl = "https://localhost:7061/api/Products/";
-        private string colorUrl = "https://localhost:7061/api/Color/";
+        private string colorUrl = "https://localhost:7061/api/Colors/";
         HttpClient httpClient = new HttpClient();
 
         public IActionResult Index(string? keyword)
@@ -19,8 +19,8 @@ namespace ThriftShop.Client.Areas.Customer.Controllers
             {
                 ProductClientModel productsVM = new ProductClientModel
                 {
-                    Products = JsonConvert.DeserializeObject<IEnumerable<Product>>(httpClient.GetStringAsync(productUrl).Result),
-                    Categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(httpClient.GetStringAsync(categoryUrl).Result),
+                    Products = JsonConvert.DeserializeObject<IEnumerable<Product>>(httpClient.GetStringAsync(productUrl + "GetAll/").Result),
+                Categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(httpClient.GetStringAsync(categoryUrl).Result),
                     Colors = JsonConvert.DeserializeObject<IEnumerable<Color>>(httpClient.GetStringAsync(colorUrl).Result),
                 };
                 return View(productsVM);
