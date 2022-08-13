fa5a2a897e4e35094b7ca36445cd7685d94f7e3b
tinhtvt97 <85776875+tinhtvt97@users.noreply.github.com>
2022-08-13 19:30:13 +0700

    ok

--- ThriftShop/ThriftShop.Client/Areas/Admin/Controllers/HomeController.cs
+++ ThriftShop/ThriftShop.Client/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Net.Http;
 using ThriftShop.Models;
@@ -9,12 +10,18 @@ namespace ThriftShop.Client.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment environment;
+        public HomeController(IWebHostEnvironment _environment)
+        {
+            environment = _environment;
+        }
 
         private string urlCategories = "https://localhost:7061/api/Categories/";
         private string urlProducts = "https://localhost:7061/api/Products/";
         private string urlColor = "https://localhost:7061/api/Colors/";
         private string urlSize = "https://localhost:7061/api/Sizes/";
         private string urlCoupon = "https://localhost:7061/api/Coupon/";
+        private string urlProductImage = "https://localhost:7061/api/ProductImages";
         private HttpClient client = new HttpClient();
 
         [Route("Admin/Home")]
@@ -197,5 +204,6 @@ namespace ThriftShop.Client.Areas.Admin.Controllers
                 return View();
             }
         }
+        
     }
 }
